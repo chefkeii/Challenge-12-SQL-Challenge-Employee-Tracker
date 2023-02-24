5897f1b27d82a19f666da330dc31e8c92b3a79bb
Kela <chefkeii@keiidesserts.com>
2023-02-23 20:44:28 -0600

    updated project

--- db/schema.sql
+++ db/schema.sql
@@ -1,11 +1,33 @@
-DROP DATABASE IF EXISTS classlist_db;
-CREATE DATABASE classlist_db;
+DROP DATABASE IF EXISTS company_db;
+CREATE DATABASE company_db;
 
-USE classlist_db;
+USE company_db;
 
-CREATE TABLE students (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  enrolled BOOLEAN NOT NULL
+CREATE TABLE departments (
+  id INT AUTO_INCREMENT NOT NUL,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
+
+CREATE TABLE role (
+id INT AUTO_INCREMENT NOT NULL,
+title VARCHAR(30) NOT NULL,
+salary DECIMAL(98,0) NOT NULL,
+department_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY (department_id),
+REFERENCES departments(id)
+);
+
+CREATE TABLE employee (
+id INT AUTO_INCREMENT NOT NUL,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT NOT NULL,
+manager_id INT,
+PRIMARY KEY (id),
+FOREIGN KEY (role_id),
+REFERENCES role (id),
+REFERENCES employee(id)
+);
+    
--- db/seed.sql
+++ db/seed.sql
@@ -1,22 +1,25 @@
-use db_name;
+USE business_db
 
-SELECT * FROM table1;
-SELECT * FROM table 2;
-SELECT * FROM table 3;
+INSERT INTO departments (name)
+VALUES ("Executive Officers"),
+       ("Sales"),
+       ("Project Management"),
+       ("Accounting"),
+       ("Human Resources"),
+       ("Scrum Team");
 
-INSERT INTO department
-    (name)
-    VALUES  
-("Sales")
-("Marketing")
-("Hiring");
-    
-    INSERT INTO role
-        (title, salary, department_id)
-        VALUES
-        ("Sales Manger, 78000, 1")
+INSERT INTO role (title, salary, department_id)
+VALUES ('Lead Project Manager', 40000, 1),
+       ('Sales Lead', 80000, 2),
+       ('Scrum Master', 105000, 3),
+       ('Accountant', 90000, 4),
+       ('HR Rep', 65000, 5),
+       ('Boss AKA CEO', 175000, 6);
 
-        INSERT INTO employee
-        (first_name, last_name, role_id, manager_id)
-        VALUES
-        ("Ann", "Derrick", 1, 1)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Ryan', 'Johnson', 6, NULL),
+       ('Jane','Doe', 1, 1),
+       ('John', 'Smith', 2, 1),
+       ('Emily', 'Brown', 3, 1),
+       ('Benjamin', 'Davis', 4, 1),
+       ('Sophie', 'Lee', 5, 1);
